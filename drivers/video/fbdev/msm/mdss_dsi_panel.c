1581b5e96e065f962975c6906dd21c7695cc0de4
Siddharth Bharadwaj <siddharthbharadwaj45@gmail.com>
2020-02-17 10:56:51 +0000

    msm: mdss: Fix some missing stuff

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -41,6 +41,8 @@ EXPORT_SYMBOL(g_lcd_id);
 extern bool enable_gesture_mode;
 #endif
 
+bool ESD_TE_status = false;
+DEFINE_LED_TRIGGER(bl_led_trigger);
 static bool ce_enable = true;
 static bool srgb_enable = true;
 static bool cabc_enable = false;
