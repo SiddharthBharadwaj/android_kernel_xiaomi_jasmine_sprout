8689bb4f0305985a7f951b451e7672b1adce012c
Siddharth Bharadwaj <siddharthbharadwaj45@gmail.com>
2020-08-06 06:38:47 +0000

    Derp

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -46,7 +46,6 @@
 #include <linux/file.h>
 #include <linux/kthread.h>
 #include <linux/dma-buf.h>
-#include <linux/devfreq_boost.h>
 #include <sync.h>
 #include <sw_sync.h>
 #ifdef CONFIG_MACH_MI
@@ -4425,7 +4424,6 @@ static int mdss_fb_set_par(struct fb_info *info)
 		mfd->panel_info->out_format = out_format;
 		if (old_format != mfd->panel_info->out_format)
 			mfd->panel_reconfig = true;
-		mfd->panel_info->out_format = new_format;
 	}
 
 	if (mdss_fb_is_hdmi_primary(mfd) && mfd->panel_reconfig)
@@ -5411,7 +5409,6 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
